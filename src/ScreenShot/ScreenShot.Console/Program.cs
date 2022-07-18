8e01e28105a4dd24d1f28416c01e1f24de8067f8
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-07-18 23:36:57 +0530

    Added the required files

--- src/ScreenShot/ScreenShot.Console/Program.cs
+++ src/ScreenShot/ScreenShot.Console/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Console.WriteLine("Hello, World!");
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ScreenShot.Console;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    DirectoryInfo di = new DirectoryInfo(@"D:\\ss");
+    if (!di.Exists) { di.Create(); }
+
+
+    while (true)
+    {
+      PrintScreen ps = new PrintScreen();
+      ps.CaptureScreenToFile(di + $"\\screenShoot{Guid.NewGuid()}.png", ImageFormat.Png);
+      Thread.Sleep(10000);
+
+    }
+  }
+}
